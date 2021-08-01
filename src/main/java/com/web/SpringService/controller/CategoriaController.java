b63bb11c3dd4db753fd82ddc8441ba0ad4fce898
JonasMoura-J <jonasmourat9090@gmail.com>
2021-08-01 01:32:27 -0300

    Adicionando metodo post para a Categoria

--- src/main/java/com/web/SpringService/controller/CategoriaController.java
+++ src/main/java/com/web/SpringService/controller/CategoriaController.java
@@ -1,11 +1,16 @@
 package com.web.SpringService.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.web.SpringService.domain.Categoria;
 import com.web.SpringService.service.CategoriaService;
@@ -23,5 +28,13 @@ public class CategoriaController {
 		Categoria obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri =  ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
