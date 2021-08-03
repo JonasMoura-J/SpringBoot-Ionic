52dfccc6b92ddb2ba0bccf825ab08afefc385c92
JonasMoura-J <jonasmourat9090@gmail.com>
2021-08-03 11:10:39 -0300

    Personalizando o retorno da validação

--- src/main/java/com/web/SpringService/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/com/web/SpringService/controller/exception/ControllerExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +28,15 @@ public class ControllerExceptionHandler {
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), LocalDateTime.now());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", LocalDateTime.now());
+		FieldError objetoErro = e.getBindingResult().getFieldError();
+		err.addError(objetoErro.getField(), objetoErro.getDefaultMessage());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
