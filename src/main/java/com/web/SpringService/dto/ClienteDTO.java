d9a53df3b53fa75c761ceb5aff337c03a9b209bb
JonasMoura-J <jonasmourat9090@gmail.com>
2021-08-04 08:29:56 -0300

    Adicionando CRUD para Cliente

---
+++ src/main/java/com/web/SpringService/dto/ClienteDTO.java
@@ -0,0 +1,59 @@
+package com.web.SpringService.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.web.SpringService.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotNull(message = "Preenchimento obrigatório")
+	@Length(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotNull(message = "Preenchimento obrigatório")
+	@Email(message="email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente cliente) {
+		id = cliente.getId();
+		nome = cliente.getNome();
+		email = cliente.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- src/main/java/com/web/SpringService/service/ClienteService.java
+++ src/main/java/com/web/SpringService/service/ClienteService.java
@@ -1,12 +1,19 @@
 package com.web.SpringService.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.web.SpringService.domain.Cliente;
+import com.web.SpringService.dto.ClienteDTO;
 import com.web.SpringService.repositories.ClienteRepository;
+import com.web.SpringService.service.exceptions.DataIntegrityException;
 import com.web.SpringService.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,5 +27,43 @@ public class ClienteService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado " + id + ", Tipo: "
 				+ Cliente.class.getName()));
+	}
+	
+	public List<Cliente> findAll(){
+		return repository.findAll();
+	}
+
+	public Page<Cliente> findPaged(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of (page, linesPerPage,  Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+	
+	public void update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		repository.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um Cliente que posssui pedidos");
+		}
+	}
+	
+	public Cliente dtoToCategotia(ClienteDTO dto) {
+		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
