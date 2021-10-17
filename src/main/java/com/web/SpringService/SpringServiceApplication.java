f359113a6629c25aab4ff60ffff65e2d0c39fea0
JonasMoura-J <jonasmourat9090@gmail.com>
2021-10-17 18:45:30 -0300

    Adicionando configs da S# e salvando primeiro arquivo

--- src/main/java/com/web/SpringService/SpringServiceApplication.java
+++ src/main/java/com/web/SpringService/SpringServiceApplication.java
@@ -1,12 +1,24 @@
 package com.web.SpringService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.web.SpringService.service.S3Service;
+
 @SpringBootApplication
-public class SpringServiceApplication {
+public class SpringServiceApplication implements CommandLineRunner{
+
+	@Autowired
+	S3Service s3Service;
 	
 	public static void main(String[] args) {	
 		SpringApplication.run(SpringServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uplodFile("C:\\temp\\Curriculo.pdf");
 	}
 }
