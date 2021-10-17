faf22c78050836aff94beff85edcba82be31911e
JonasMoura-J <jonasmourat9090@gmail.com>
2021-10-17 13:52:22 -0300

    Restição de conteudos, o usuario recupera apenas o seu pedido

--- src/main/java/com/web/SpringService/repositories/PedidoRepository.java
+++ src/main/java/com/web/SpringService/repositories/PedidoRepository.java
@@ -1,9 +1,16 @@
 package com.web.SpringService.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.web.SpringService.domain.Cliente;
 import com.web.SpringService.domain.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/web/SpringService/service/PedidoService.java
+++ src/main/java/com/web/SpringService/service/PedidoService.java
@@ -4,23 +4,29 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.web.SpringService.domain.Cliente;
 import com.web.SpringService.domain.PagamentoComBoleto;
 import com.web.SpringService.domain.Pedido;
 import com.web.SpringService.domain.enums.SituacaoPagamento;
 import com.web.SpringService.repositories.ItemPedidoRepository;
 import com.web.SpringService.repositories.PagamentoRepository;
 import com.web.SpringService.repositories.PedidoRepository;
+import com.web.SpringService.security.UserSpringSecurity;
 import com.web.SpringService.service.email.EmailService;
+import com.web.SpringService.service.exceptions.AuthorizationException;
 import com.web.SpringService.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
 	
 	@Autowired
-	PedidoRepository repository;
+	PedidoRepository pedidoRepository;
 	@Autowired
 	PagamentoRepository pagamentoRepository;
 	@Autowired
@@ -35,7 +41,7 @@ public class PedidoService {
 	EmailService emailService;
 	
 	public Pedido buscar(Integer id) {
-		Optional<Pedido> pedido = repository.findById(id);
+		Optional<Pedido> pedido = pedidoRepository.findById(id);
 		return pedido.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
@@ -53,7 +59,7 @@ public class PedidoService {
 			PagamentoComBoleto pagamento = (PagamentoComBoleto) pedido.getPagamento();
 			boletoService.preecherPagamentoComBoleto(pagamento, pedido.getInstante());
 		}
-		repository.save(pedido);
+		pedidoRepository.save(pedido);
 		pagamentoRepository.save(pedido.getPagamento());
 		
 		pedido.getItens().stream().forEach(x -> {
@@ -66,5 +72,17 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(pedido.getItens());
 		emailService.sendOrderConfirmationEmail(pedido.getCliente().getEmail(), "Pedido Confirmado", pedido);
 		return pedido;
+	}
+	
+	public Page<Pedido> findPaged(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSpringSecurity user = UserService.autenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,  Direction.valueOf(direction), orderBy);
+		
+		Cliente cliente = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 }
